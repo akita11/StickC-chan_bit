b7251df63f87a14e148fb042d03fd05feba7184f
akita11 <akita@ifdl.jp>
2025-01-07 16:57:32 +0900

    face, speak (to be rotated)

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ uint8_t st_blink = 0, st_mouth = 0;
 uint8_t fSpeaking = 0;
 
 uint8_t face_fw = 16, face_fh = 8;
-uint16_t face_ww = M5.Display.width(), face_wh = M5.Display.height();
+uint16_t face_ww, face_wh;
 uint8_t face_eye_x = 90, face_eye_y = 80, face_eye_r = 10;
 uint8_t face_eye_close_width  = 6, face_eye_close_height = 40;
 uint8_t face_mouth_x = 40, face_mouth_y = 120, face_mouth_width = 7, face_mouth_height = 50, face_mouth_open_width = 30;
@@ -29,13 +29,13 @@ void _breath()
 }
 
 void _eye_close(){
-	M5.Display.drawCircle(face_eye_x + dev, face_eye_y, face_eye_r+10, TFT_BLACK);
-	M5.Display.drawCircle(face_eye_x + dev, face_wh - face_eye_y, face_eye_r+10, TFT_BLACK);
-	M5.Display.drawRect(
+	M5.Display.fillCircle(face_eye_x + dev, face_eye_y, face_eye_r+10, TFT_BLACK);
+	M5.Display.fillCircle(face_eye_x + dev, face_wh - face_eye_y, face_eye_r+10, TFT_BLACK);
+	M5.Display.fillRect(
         face_eye_x - face_eye_close_width / 2 + dev,
         face_eye_y - face_eye_close_height / 2,
         face_eye_close_width, face_eye_close_height, TFT_WHITE);
-	M5.Display.drawRect(
+	M5.Display.fillRect(
         face_eye_x - face_eye_close_width / 2 + dev,
         face_wh - face_eye_y - face_eye_close_height / 2,
         face_eye_close_width, face_eye_close_height,
@@ -43,17 +43,16 @@ void _eye_close(){
 }
 
 void _eye_open(){
-	M5.Display.drawRect(
+	M5.Display.fillRect(
         face_eye_x - face_eye_close_width / 2 + dev - 10,
         face_eye_y - face_eye_close_height / 2,
         face_eye_close_width + 20, face_eye_close_height, TFT_BLACK);
-	M5.Display.drawRect(
+	M5.Display.fillRect(
         face_eye_x - face_eye_close_width / 2 + dev - 10,
         face_wh - face_eye_y - face_eye_close_height / 2,
-        face_eye_close_width + 20, face_eye_close_height,
-        TFT_BLACK);
-	M5.Display.drawCircle(face_eye_x + dev, face_eye_y, face_eye_r, TFT_WHITE);
-	M5.Display.drawCircle(face_eye_x + dev, face_wh - face_eye_y, face_eye_r, TFT_WHITE);
+        face_eye_close_width + 20, face_eye_close_height, TFT_BLACK);
+	M5.Display.fillCircle(face_eye_x + dev, face_eye_y, face_eye_r, TFT_WHITE);
+	M5.Display.fillCircle(face_eye_x + dev, face_wh - face_eye_y, face_eye_r, TFT_WHITE);
 }
 
 void _blink()
@@ -62,6 +61,7 @@ void _blink()
 	if (st_blink == 0){
 		_eye_close();
 		if (tm_blink >= tm_blink_open){
+			tm_blink_open = random(4, 10) * 500;
 			st_blink = 1;
 			tm_blink = 0;
 		}
@@ -69,7 +69,7 @@ void _blink()
 	else{
   	_eye_open();
   	if (tm_blink >= tm_blink_open){
-			tm_blink_open = random(2, 6) * 1000;
+      tm_blink_open = 500;
 			st_blink = 0;
 			tm_blink = 0;
 		}
@@ -92,17 +92,17 @@ void _mouth(){
     face_mouth_y - face_mouth_height / 2,
     face_mouth_width,
     face_mouth_height,
-    TFT_BLACK);
+    TFT_WHITE);
 }
 
 void _lip_open(){
-  M5.Display.drawRect(
+  M5.Display.fillRect(
     face_mouth_x - face_mouth_open_width / 2,
     face_mouth_y - face_mouth_height / 2,
     face_mouth_open_width,
     face_mouth_height,
     TFT_BLACK);
-  M5.Display.drawRect(
+  M5.Display.fillRect(
     face_mouth_x - face_mouth_open_width / 2,
     face_mouth_y - face_mouth_height / 2,
     face_mouth_open_width,
@@ -112,13 +112,13 @@ void _lip_open(){
 }
 
 void _lip_close(){
-  M5.Display.drawRect(
+  M5.Display.fillRect(
     face_mouth_x - face_mouth_open_width / 2,
     face_mouth_y - face_mouth_height / 2,
     face_mouth_open_width,
     face_mouth_height,
     TFT_BLACK);
-  M5.Display.drawRect(
+  M5.Display.fillRect(
     face_mouth_x - face_mouth_width / 2,
     face_mouth_y - face_mouth_height / 2,
     face_mouth_width,
@@ -134,34 +134,65 @@ void _lipsync(){
 
 void _speak(String str){
 	fSpeaking = 1;
-	tm_speak = 0;
+  M5.Display.setTextSize(2);
+  M5.Display.setCursor(24, 20); M5.Display.print(str);
 	_lipsync();
-	while(tm_speak < tm_speak_period);
 	tm_speak = 0;
-
-	// ToDo
+  delay(tm_speak_period);
+  while(str.length() > 0){
+    str = str.substring(1, str.length());
+    M5.Display.clear();
+    M5.Display.setCursor(24, 20);
+    M5.Display.print(str);
+    _lipsync();
+    delay(tm_speak_period);
+  }
+  M5.Display.clearDisplay();
 
   _lip_close();
   fSpeaking = 0;
 }
 
-void _exclamation_color(uint32_t color){
-	// ToDo
+void _exclamation_color(uint16_t color){
+  M5.Display.fillRect(
+    face_exclamation_x,
+    face_exclamation_y,
+    face_exclamation_height,
+    face_exclamation_height,
+    color
+  );
+  M5.Display.fillRect(face_exclamation_x + face_exclamation_height + face_exclamation_space, face_exclamation_y, face_exclamation_width, face_exclamation_height, color);
 }
 
-void _pale_color(uint32_t color){
-	// ToDo
+void _pale_color(uint16_t color){
+   M5.Display.fillRect(95, 220, 40, 5, color);
+   M5.Display.fillRect(90, 210, 45, 5, color);
+   M5.Display.fillRect(85, 200, 50, 5, color);
+   M5.Display.fillRect(80, 190, 55, 5, color);
 }
 
-void _angry_color(uint32_t color){
-	// ToDo
+void _angry_color(uint16_t color){
+  M5.Display.fillRect( face_angry_x - face_angry_gap/2 - face_angry_length, face_angry_y - face_angry_gap/2 - face_angry_width,
+    face_angry_length, face_angry_width, color);
+  M5.Display.fillRect( face_angry_x - face_angry_gap/2 - face_angry_width/2, face_angry_y - face_angry_gap/2 - face_angry_length,
+    face_angry_width, face_angry_length, color);
+  M5.Display.fillRect( face_angry_x + face_angry_gap/2, face_angry_y - face_angry_gap/2 - face_angry_width,
+    face_angry_length, face_angry_width, color);
+  M5.Display.fillRect( face_angry_x + face_angry_gap/2, face_angry_y - face_angry_gap/2 - face_angry_width - face_angry_length,
+    face_angry_width, face_angry_length, color);
+  M5.Display.fillRect( face_angry_x - face_angry_gap/2 - face_angry_length, face_angry_y + face_angry_gap/2 - face_angry_width,
+    face_angry_length, face_angry_width, color);
+  M5.Display.fillRect( face_angry_x - face_angry_gap/2 - face_angry_width/2, face_angry_y + face_angry_gap/2,
+    face_angry_width, face_angry_length, color);
+  M5.Display.fillRect( face_angry_x + face_angry_gap/2, face_angry_y + face_angry_gap/2 - face_angry_width,
+    face_angry_length, face_angry_width, color);
+  M5.Display.fillRect( face_angry_x + face_angry_gap/2, face_angry_y + face_angry_gap/2 - face_angry_width,
+    face_angry_width, face_angry_length, color);
 }
-
 
 void IRAM_ATTR onTimer() {
   _breath();
   _blink();
-  tm_speak = tm_speak + TIMER_INTERVAL;
 }
 
 void setup() {
@@ -173,6 +204,22 @@ void setup() {
   timerAlarmWrite(timer, TIMER_INTERVAL * 1000, true); // every 100ms
   timerAlarmEnable(timer);
 	_lip_close();
+  face_ww = M5.Display.width(); face_wh = M5.Display.height();
 }
 void loop() {
+  _speak("hogehoge");
+/*
+  _angry_color(TFT_RED);
+  delay(1000);
+  _angry_color(TFT_BLACK);
+  delay(1000);
+  _pale_color(TFT_BLUE);
+  delay(1000);
+  _pale_color(TFT_BLACK);
+  delay(1000);
+  _exclamation_color(TFT_YELLOW);
+  delay(1000);
+  _exclamation_color(TFT_BLACK);
+  delay(1000);
+*/
 }
