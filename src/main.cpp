119a58f7216c9c080f5d72cb6171cb91515463be
Junichi Akita <akita@ifdl.jp>
2025-01-07 19:05:08 +0900

    rotate display

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,19 @@ uint16_t tm_blink = 0, tm_mouth = 0, tm_speak = 0, tm_blink_open = 500, tm_mouth
 uint8_t breath_T = 0, breath_cycle = 30, breath_dev = 4;
 int8_t dev = 0;
 uint8_t st_blink = 0, st_mouth = 0;
-uint8_t fSpeaking = 0;
 
 uint8_t face_fw = 16, face_fh = 8;
 uint16_t face_ww, face_wh;
-uint8_t face_eye_x = 90, face_eye_y = 80, face_eye_r = 10;
-uint8_t face_eye_close_width  = 6, face_eye_close_height = 40;
-uint8_t face_mouth_x = 40, face_mouth_y = 120, face_mouth_width = 7, face_mouth_height = 50, face_mouth_open_width = 30;
-uint8_t face_exclamation_x = 60, face_exclamation_y = 220, face_exclamation_width  = 30, face_exclamation_height = 10, face_exclamation_space  = 8;
-uint8_t face_angry_x = 110, face_angry_y = 210, face_angry_width = 2, face_angry_length = 10, face_angry_gap = 10;
+//uint8_t face_eye_x = 90, face_eye_y = 80, face_eye_r = 10;
+uint8_t face_eye_y = 90, face_eye_x = 80, face_eye_r = 10;
+//uint8_t face_eye_close_width  = 6, face_eye_close_height = 40;
+uint8_t face_eye_close_width  = 40, face_eye_close_height = 6;
+//uint8_t face_mouth_x = 40, face_mouth_y = 120, face_mouth_width = 7, face_mouth_height = 50, face_mouth_open_width = 30;
+uint8_t face_mouth_x = 120, face_mouth_y = 40, face_mouth_width = 50, face_mouth_height = 7, face_mouth_open_width = 30;
+//uint8_t face_exclamation_x = 60, face_exclamation_y = 220, face_exclamation_width = 30, face_exclamation_height = 10, face_exclamation_space  = 8;
+uint8_t face_exclamation_x = 220, face_exclamation_y = 60, face_exclamation_width = 10, face_exclamation_height = 30, face_exclamation_space  = 8;
+//uint8_t face_angry_x = 110, face_angry_y = 210, face_angry_width = 2, face_angry_length = 10, face_angry_gap = 10;
+uint8_t face_angry_x = 210, face_angry_y = 110, face_angry_width = 10, face_angry_length = 2, face_angry_gap = 10;
 
 #define TIMER_INTERVAL 100 // [ms]
 
@@ -32,25 +36,24 @@ void _eye_close(){
 	M5.Display.fillCircle(face_eye_x + dev, face_eye_y, face_eye_r+10, TFT_BLACK);
 	M5.Display.fillCircle(face_eye_x + dev, face_wh - face_eye_y, face_eye_r+10, TFT_BLACK);
 	M5.Display.fillRect(
-        face_eye_x - face_eye_close_width / 2 + dev,
-        face_eye_y - face_eye_close_height / 2,
-        face_eye_close_width, face_eye_close_height, TFT_WHITE);
+		face_eye_x - face_eye_close_width / 2 + dev,
+		face_eye_y - face_eye_close_height / 2,
+		face_eye_close_width, face_eye_close_height, TFT_WHITE);
 	M5.Display.fillRect(
-        face_eye_x - face_eye_close_width / 2 + dev,
-        face_wh - face_eye_y - face_eye_close_height / 2,
-        face_eye_close_width, face_eye_close_height,
-        TFT_WHITE);
+		face_eye_x - face_eye_close_width / 2 + dev,
+		face_wh - face_eye_y - face_eye_close_height / 2,
+		face_eye_close_width, face_eye_close_height, TFT_WHITE);
 }
 
 void _eye_open(){
 	M5.Display.fillRect(
-        face_eye_x - face_eye_close_width / 2 + dev - 10,
-        face_eye_y - face_eye_close_height / 2,
-        face_eye_close_width + 20, face_eye_close_height, TFT_BLACK);
+		face_eye_x - face_eye_close_width / 2 + dev - 10,
+		face_eye_y - face_eye_close_height / 2,
+		face_eye_close_width + 20, face_eye_close_height, TFT_BLACK);
 	M5.Display.fillRect(
-        face_eye_x - face_eye_close_width / 2 + dev - 10,
-        face_wh - face_eye_y - face_eye_close_height / 2,
-        face_eye_close_width + 20, face_eye_close_height, TFT_BLACK);
+		face_eye_x - face_eye_close_width / 2 + dev - 10,
+		face_wh - face_eye_y - face_eye_close_height / 2,
+		face_eye_close_width + 20, face_eye_close_height, TFT_BLACK);
 	M5.Display.fillCircle(face_eye_x + dev, face_eye_y, face_eye_r, TFT_WHITE);
 	M5.Display.fillCircle(face_eye_x + dev, face_wh - face_eye_y, face_eye_r, TFT_WHITE);
 }
@@ -82,7 +85,7 @@ void _mouth(){
 		tm_mouth = 0;
 	}
   M5.Display.drawRect(
-    face_mouth_x - face_mouth_width / 2,
+		face_mouth_x - face_mouth_width / 2,
     face_mouth_y - face_mouth_height / 2,
     face_mouth_width + 20,
     face_mouth_height,
@@ -133,24 +136,21 @@ void _lipsync(){
 }
 
 void _speak(String str){
-	fSpeaking = 1;
   M5.Display.setTextSize(2);
-  M5.Display.setCursor(24, 20); M5.Display.print(str);
+//  M5.Display.setCursor(24, 20); M5.Display.print(str);
+  M5.Display.setCursor(20, face_wh - 20); M5.Display.print(str);
 	_lipsync();
 	tm_speak = 0;
   delay(tm_speak_period);
   while(str.length() > 0){
-    str = str.substring(1, str.length());
-    M5.Display.clear();
-    M5.Display.setCursor(24, 20);
-    M5.Display.print(str);
-    _lipsync();
-    delay(tm_speak_period);
+		str = str.substring(1, str.length());
+		M5.Display.clear();
+		M5.Display.setCursor(20, face_wh - 20); M5.Display.print(str);
+		_lipsync();
+		delay(tm_speak_period);
   }
-  M5.Display.clearDisplay();
-
+	M5.Display.clearDisplay();
   _lip_close();
-  fSpeaking = 0;
 }
 
 void _exclamation_color(uint16_t color){
@@ -172,10 +172,10 @@ void _pale_color(uint16_t color){
 }
 
 void _angry_color(uint16_t color){
-  M5.Display.fillRect( face_angry_x - face_angry_gap/2 - face_angry_length, face_angry_y - face_angry_gap/2 - face_angry_width,
-    face_angry_length, face_angry_width, color);
+	M5.Display.fillRect( face_angry_x - face_angry_gap/2 - face_angry_length, face_angry_y - face_angry_gap/2 - face_angry_width,
+		face_angry_length, face_angry_width, color);
   M5.Display.fillRect( face_angry_x - face_angry_gap/2 - face_angry_width/2, face_angry_y - face_angry_gap/2 - face_angry_length,
-    face_angry_width, face_angry_length, color);
+  	face_angry_width, face_angry_length, color);
   M5.Display.fillRect( face_angry_x + face_angry_gap/2, face_angry_y - face_angry_gap/2 - face_angry_width,
     face_angry_length, face_angry_width, color);
   M5.Display.fillRect( face_angry_x + face_angry_gap/2, face_angry_y - face_angry_gap/2 - face_angry_width - face_angry_length,
@@ -198,6 +198,8 @@ void IRAM_ATTR onTimer() {
 void setup() {
   auto cfg = M5.config();
   M5.begin(cfg);
+  // https://lang-ship.com/blog/work/m5stack-m5stickc-plus2-1/
+  M5.Display.setRotation(1); 
   Serial2.begin(115200, SERIAL_8N1, 26, 0); // RX=26, TX=0
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
